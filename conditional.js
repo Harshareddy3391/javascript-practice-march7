650be0928ee8b868aa15aa2a8e503a1c6e1b5273
Harshareddy3391 <thotaharshareddy@gmail.com>
2026-03-06 13:34:29 +0530

    add javascript practice files

---
+++ conditional.js
@@ -0,0 +1,31 @@
+if("salman"){
+    console.log("salman bachelor")
+}else{
+    console.log("we dont know")
+}
+
+
+
+
+if(1000){
+    console.log("good morning")
+}else{
+    console.log("gn")
+}
+
+
+
+ if(_){
+    console.log("under")
+}else{
+    console.log("else")
+}
+
+let price
+
+if(price*0){
+    console.log("salman bachelor")
+}else{
+    console.log("we dont know")
+}
+
---
+++ loops.js
@@ -0,0 +1,29 @@
+for(let i=7;i<=7;i++){
+     for(let j=1;j<=10;j++)
+         
+        console.log(`${i}*${j}=${i*j} `);
+
+    
+}
+
+
+
+
+let arry=["one" , "two", "three" , "four"]
+
+
+for(let i=0;i<=3;i++){
+    console.log(arry[i])
+}
+
+let ok="okkk"
+
+
+
+console.log(ok)
+//for of
+
+for(let n of arry){
+   
+    console.log(n)
+}
