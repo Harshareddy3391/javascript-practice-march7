b8b6f6173b76d5055c2ac86055d9ebb3b710429d
Harshareddy3391 <thotaharshareddy@gmail.com>
2026-03-10 10:45:27 +0530

    functions

--- function.js
+++ function.js
@@ -8,4 +8,29 @@ function login(){
 login();
 login();
 login();
-login();
+login(3,2);
+
+
+
+function add(a,b){
+    console.log( a+b)
+}
+
+
+add(2,4)
+add(2,"harsha")
+add(2,"30")
+add(2)
+
+add(1,2,3)
+
+
+function adi(a,b,c=2){
+    console.log(a+b+c)
+}
+
+adi(12,32,22)
+adi(12,33)
+adi(12,"hello")
+adi("rahul")
+adi(4)
