7d4dd48b77122b115651a3043f5aecc48c658459
Harshareddy3391 <thotaharshareddy@gmail.com>
2026-03-11 12:28:50 +0530

    dom

--- events.js
+++ events.js
@@ -1,14 +1,20 @@
-function msg(){
-    alert("click")
-}
-
-function time(){
-
-     let dis=document.getElementsByClassName('ptag')
-
-     document.writeln(dis)
-
-     dis.innerHTML="HARSHA"
-}
+ function uday(){
+    alert("hi uday")
+ }
 
 
+ function date(){
+    let currentdate=new Date()
+
+    document.writeln(currentdate)
+ }
+
+
+
+ function time(){
+    let ct=new Date();
+
+   document.getElementById('ptag').innerHTML=ct;
+
+   
+ }
