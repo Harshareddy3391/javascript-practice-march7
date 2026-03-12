e97fb1f27ceecac3b941f10380c2a492ebbba36c
Harshareddy3391 <thotaharshareddy@gmail.com>
2026-03-12 10:30:54 +0530

    bu

---
+++ event.js
@@ -0,0 +1,12 @@
+function green(){
+    
+    let d=document.getElementById('mo')
+    d.style.background="green"
+}
+
+
+
+function blue(){
+    document.getElementById('mout').style.background="blue"
+}
+
