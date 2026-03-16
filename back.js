295d1f9ad86b21aa95065d00ca9370d37fa6ec6f
Harshareddy3391 <thotaharshareddy@gmail.com>
2026-03-16 10:44:28 +0530

    c

--- back.js
+++ back.js
@@ -1,4 +1,15 @@
-function change(){
+function change1(){
 
     //alert("harsha")
+     document.getElementById('ip1').style.background="yellow"
+}
+
+function change2(){
+     document.getElementById('ip2').style.background="green"
+    
+}
+
+function change3(){
+    document.getElementById('ip3').style.textTransform="uppercase"
+
 }
